811524e8d651afd223160a7ead4f839a1d65e4ce
Mauricio Forero <mauricio.forero@novatec.com.co>
2023-09-29 17:12:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pragmaCats',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
